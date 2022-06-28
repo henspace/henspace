0fabe2e45c0d7cecae221c285d176e687dac197e
Steve Butler <henspace@users.noreply.github.com>
2022-06-28 16:18:25 +0100

    Setup starting point for CourseEngine script

---
+++ codingCourse/scripts/courseEngine.js
@@ -0,0 +1,9 @@
+class CourseEngineClass {
+	NAME_ = "Coding Course Engine";
+	VERSION_ = "1.0.0 2022-06-28";
+	
+  showVersion() {
+  	alert(`${this.NAME_}\nVersion: ${this.VERSION_}`);
+  }
+}
+CourseEngine = new CourseEngineClass();
