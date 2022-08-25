d5a883964a01e0e88e2e5301352f08b0242f5d3d
Steve Butler <henspace@users.noreply.github.com>
2022-08-25 20:19:57 +0100

    Remove Kenny source and just leave atlas

--- codingCourse/scripts/courseEngine.js
+++
@@ -1,9 +0,0 @@
-class CourseEngineClass {
-	static NAME_ = "Coding Course Engine";
-	static VERSION_ = "1.0.0 2022-06-28";
-	
-  showVersion() {
-  	alert(`${CourseEngineClass.NAME_}\nVersion: ${CourseEngineClass.VERSION_}`);
-  }
-}
-CourseEngine = new CourseEngineClass();
